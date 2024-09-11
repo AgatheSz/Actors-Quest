b47a7930d4a1bdfd2b50bb39e79ca8ae799555f1
Agathe Szkola <agathe.szkola@gmail.com>
2024-09-11 11:03:29 +0200

    fetch recherche de personnalité

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import API_KEY from "./config.js";
+
 /**
  * VARIABLES
  **/
@@ -7,7 +9,7 @@ const searchInput = document.getElementById('search-bar');
 const searchResultDiv = document.getElementById('search-result');
 
 /**
- * FONCTIONS
+ * FONCTIONS 
  **/
 
 function createResultCard(data) {
@@ -17,18 +19,35 @@ function createResultCard(data) {
   resultCard.className = 'result-card';
 
   const newImg = new Image();
+  // https://image.tmdb.org/t/p/w200 + lien image
   newImg.src = data.img;
   resultCard.appendChild(newImg);
 
   const newP = document.createElement('p');
-  newP.textContent = `${data.firstName} ${data.lastName}`;
+  newP.textContent = data.name;
   resultCard.appendChild(newP);
 }
 
-const ayoData = { img: './assets/Ayo-Edebiri-Photo-Credit-Myles-Loftin-2.png', firstName: 'Ayo', lastName: 'Edebiri' };
+function fetchPerson(searchTerm) {
+  fetch(`https://api.themoviedb.org/3/search/person?query=${searchTerm}&api_key=${API_KEY}`)
+    .then((response) => {
+      response.json()
+        .then((usersData) => {
+          console.log(usersData);
+        });
+    });
+}
+
+const ayoData = { img: './assets/img/Ayo-Edebiri-Photo-Credit-Myles-Loftin-2.png', name: 'Ayo Edebiri' };
+const cateData = { img: './assets/img/cate-blanchett.png', name: 'Cate Blanchett' };
 
 /**
  * MAIN SCRIPT
  **/
 
 createResultCard(ayoData);
+createResultCard(cateData);
+
+searchBtn.addEventListener('click', () => {
+  fetchPerson(searchInput.value);
+})
