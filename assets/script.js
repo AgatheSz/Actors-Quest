b98cb27d4c82136c10ea9d041a79c9c8b773e402
Agathe Szkola <agathe.szkola@gmail.com>
2025-02-04 14:11:08 +0100

    retour à l'ancienne méthode de gestion des favoris

--- assets/script.js
+++ assets/script.js
@@ -9,22 +9,14 @@ const searchInput = document.getElementById('search-bar');
 const searchResultDiv = document.getElementById('search-result');
 const searchHistoryDiv = document.getElementById('search-history');
 const detailsDiv = document.getElementById('person-details');
+const aside = document.querySelector('aside');
 const ulMovies = document.querySelector('#movies-and-tv>ul:first-child');
 const ulTv = document.querySelector('#movies-and-tv>ul:last-child');
-const ulFav = document.querySelector('aside>ul');
 
 /**
  * FONCTIONS 
  **/
 
-/**
- * Crée un nouvel élement html et le place à l'endroit voulu dans la page HTML
- * @param {HTMLElement} elt - nouvel élement
- * @param {HTMLElement} parentElt - élement parent
- * @param {String} text - contenu textuel de l'élement
- * 
- * @returns {HTMLElement} - nouvel élement
- **/
 function createElt(elt, parentElt, text) {
   const newElt = document.createElement(elt);
   newElt.textContent = text;
@@ -33,11 +25,6 @@ function createElt(elt, parentElt, text) {
   return newElt;
 }
 
-/**
- * @param {String} birthday - date de naissance au format aaaa-mm-jj
- * 
- * @returns {Date} - date de naissance au format jj mois aaaa
- **/
 function parseBirthDate(birthday) {
   const birthDate = new Date(birthday);
   birthday = birthDate.toLocaleString('default', { day: '2-digit', month: 'long', year: 'numeric' });
@@ -45,10 +32,6 @@ function parseBirthDate(birthday) {
   return birthday;
 }
 
-/**
- * @param {String} url - URL vers image acteur
- * @param {HTMLElement} - élement parent auquel l'image va être ajoutée 
- **/
 function addImage(url, parentElt) {
   const newImg = new Image();
   if (!url) {
@@ -59,11 +42,6 @@ function addImage(url, parentElt) {
   parentElt.appendChild(newImg);
 }
 
-/**
- * Création d'une carte présentant les informations des acteurs
- * @param {JSON} data - données de l'acteur récupérées via API
- * @param {HTMLElement} parentElt - élement parent auquel la carte de l'acteur va être ajoutée
- **/
 function createResultCard(data, parentElt) {
   const resultCard = createElt('div', parentElt, "");
   resultCard.className = 'result-card';
@@ -90,9 +68,6 @@ function createResultCard(data, parentElt) {
   });
 }
 
-/**
- * @returns {[HTMLElement]} 
- **/
 function restoreDivs() {
   const photoIdentityDiv = createElt('div', detailsDiv, '');
   photoIdentityDiv.id = "photo-identity";
@@ -102,10 +77,6 @@ function restoreDivs() {
   return [photoIdentityDiv, identityDiv];
 }
 
-/**
- * Affichage des réultats de la recherche d'acteur par nom
- * @param {JSON} data - données de l'acteur récupérées via API
- **/
 function displaySearchResults(data) {
   searchResultDiv.textContent = "";
   for (let i = 0; i < data.results.length; i++) {
@@ -114,11 +85,6 @@ function displaySearchResults(data) {
   }
 }
 
-/**
- * @param {String} name - nom de l'acteur
- * @param {String} profile_path - URL photo de l'acteur
- * @param {Number} id - id de l'acteur
- **/
 function saveSearchHistory(name, profile_path, id) {
   let peopleArr = sessionStorage.getItem("people");
   if (!peopleArr) {
@@ -142,9 +108,6 @@ function saveSearchHistory(name, profile_path, id) {
   handleHistory()
 }
 
-/**
- * 
- **/
 function handleHistory() {
   searchHistoryDiv.textContent = "";
   createElt('h2', searchHistoryDiv, 'Historique');
@@ -158,98 +121,42 @@ function handleHistory() {
   }
 }
 
-/**
- * @param {String} fillColor - couleur de remplissage de l'étoile
- * 
- * @returns {String} SVG de l'étoile
- **/
 function drawStar(fillColor, strokeColor) {
   return `<?xml version="1.0" encoding="utf-8"?>
 <svg width="50px" height="50px" viewBox="0 0 25 25" fill=${fillColor} xmlns="http://www.w3.org/2000/svg">
 <path d="M13 4L15.2747 9.8691L21.5595 10.2188L16.6806 14.1959L18.2901 20.2812L13 16.87L7.70993 20.2812L9.31941 14.1959L4.44049 10.2188L10.7253 9.8691L13 4Z" 
-stroke=${strokeColor} stroke-width="0.5"/>
+stroke="${strokeColor}" stroke-width="0.5"/>
 </svg>`
 }
 
-/**
- * @param {Number} personId - id de l'acteur
- * @param {String} star - étoile de favoris
- **/
 function displayStar(personId, star) {
-  let favourites = JSON.parse(localStorage.getItem('favourites'));
-  if () {
+  if (localStorage.getItem(personId)) {
     star.innerHTML = drawStar('#f3c023', '#f3c023');
   } else {
     star.innerHTML = drawStar('none', '#000');
   }
 }
 
-/**
- * @param {Number} personId - id de l'acteur
- * @param {String} personName - nom de l'acteur
- * @param {String} url - URL de la photo de l'acteur
- * @param {String} star - SVG de l'étoile
- * 
- * @returns {} 
- **/
-function handleFavouritesClic(personId, personName, url, star) {
-  let favourites = localStorage.getItem('favourites');
-
-  if (!favourites) {
-
-    favourites = [];
-
+function handleFavouritesClic(personId, personName, star) {
+  if (!localStorage.getItem(personId)) {
+    star.innerHTML = drawStar('#f3c023', '#f3c023');
+    localStorage.setItem(personId, personName);
   } else {
-
-    favArr = JSON.parse(favourites);
-
-    favArr + favArr.filter()
-    if (!favArr[personId]) {
-      favArr[personId] = { name: personName, profile_path: url };
-      star.innerHTML = drawStar('#f3c023');
-      localStorage.setItem('favourites', JSON.stringify(favArr));
-
-    } else {
-
-      star.innerHTML = drawStar('none');
-      favArr = favArr.filter((personId) => {
-        // objet.hasOwn()
-        return
-      })
-    }
+    star.innerHTML = drawStar('none', '#000');
+    localStorage.removeItem(personId);
   }
-
-  peopleArr = peopleArr.filter((person) => {
-    return person.name !== name;
-  });
 }
 
-const person = { 'id': id, 'name': name, 'profile_path': profile_path };
-if (peopleArr.length >= 3) {
-  peopleArr.pop();
-}
-}
-
-/**
- *  
- **/
 function displayFavourites() {
-  ulFav.textContent = "";
+  aside.textContent = "";
+  createElt('h2', aside, 'Liste des favoris');
 
   for (let i = 0; i < localStorage.length; i++) {
     const id = localStorage.key(i);
-    let newLi = createElt('li', ulFav, localStorage.getItem(id));
-
-    newLi.addEventListener('click', () => {
-      fetchPersonDetails(id);
-      saveSearchHistory()
-    });
+    createElt('p', aside, localStorage.getItem(id));
   }
 }
 
-/**
- * @param {JSON} data - données de l'acteur récupérées via API
- **/
 function displayDetails(data) {
   detailsDiv.textContent = "";
 
@@ -269,9 +176,6 @@ function displayDetails(data) {
   createElt('p', detailsDiv, data.biography);
 }
 
-/**
- * @param {JSON} data - données des films d'un acteur spécifique récupérées via API
- **/
 function displayMovies(data) {
   const movieData = data.cast;
   ulMovies.textContent = "";
@@ -286,9 +190,6 @@ function displayMovies(data) {
   }
 }
 
-/**
- * @param {JSON} data - données des séries d'un acteur spécifique récupérées via API
- **/
 function displayTvShows(data) {
   const tvData = data.cast;
   ulTv.textContent = "";
@@ -303,10 +204,6 @@ function displayTvShows(data) {
   }
 }
 
-/**
- * @param {JSON} data - données d'un film récupérées via API
- * @param {String} title - titre du film
- **/
 function displayMovieActors(data, title) {
   const movieCreditsData = data.cast;
   searchResultDiv.textContent = "";
@@ -318,10 +215,6 @@ function displayMovieActors(data, title) {
   }
 }
 
-/**
- * @param {JSON} data - données d'une série récupérées via API
- * @param {String} name - titre de la série
- **/
 function displayTvActors(data, name) {
   const tvCreditsData = data.cast;
   searchResultDiv.textContent = "";
@@ -333,11 +226,6 @@ function displayTvActors(data, name) {
   }
 }
 
-// ------------------------------------------> Appels API <------------------------------------------
-
-/**
- * @param {String} searchTerm - critères de recherche 
- **/
 function fetchPerson(searchTerm) {
   fetch(`https://api.themoviedb.org/3/search/person?query=${searchTerm}&api_key=${API_KEY}`)
     .then((response) => {
@@ -348,9 +236,6 @@ function fetchPerson(searchTerm) {
     });
 }
 
-/**
- * @param {Number} personId - id de l'acteur
- **/
 function fetchPersonDetails(personId) {
   fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=${API_KEY}&language=fr-FR`)
     .then((response) => {
@@ -361,9 +246,6 @@ function fetchPersonDetails(personId) {
     });
 }
 
-/**
- * @param {Number} personId - id de l'acteur
- **/
 function fetchMovies(personId) {
   fetch(`https://api.themoviedb.org/3/person/${personId}/movie_credits?language=fr-FR&api_key=${API_KEY}`)
     .then((response) => {
@@ -374,9 +256,6 @@ function fetchMovies(personId) {
     });
 }
 
-/**
- * @param {Number} personId - id de l'acteur
- **/
 function fetchTvShows(personId) {
   fetch(`https://api.themoviedb.org/3/person/${personId}/tv_credits?language=fr-FR&api_key=${API_KEY}`)
     .then((response) => {
@@ -387,10 +266,6 @@ function fetchTvShows(personId) {
     });
 }
 
-/**
- * @param {Number} movieId - id du film
- * @param {String} title - titre du film
- **/
 function fetchMovieCredits(movieId, title) {
   fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=fr-FR&api_key=${API_KEY}`)
     .then((response) => {
@@ -401,10 +276,6 @@ function fetchMovieCredits(movieId, title) {
     });
 }
 
-/**
- * @param {Number} tvShowId - id de la série
- * @param {String} name - titre de la série
- **/
 function fetchTvCredits(tvShowId, name) {
   fetch(`https://api.themoviedb.org/3/tv/${tvShowId}/credits?language=fr-FR&api_key=${API_KEY}`)
     .then((response) => {
@@ -421,7 +292,6 @@ function fetchTvCredits(tvShowId, name) {
 
 searchBtn.addEventListener('click', () => {
   fetchPerson(searchInput.value);
-  searchInput.value = "";
 });
 
 handleHistory();
